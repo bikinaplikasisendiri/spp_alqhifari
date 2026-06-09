3221887f9fe894577eaf613bbe47a40868896632
bikinaplikasisendiri <bikinaplikasisendiri@gmail.com>
2026-06-10 00:11:35 +0700

    Delete api directory

--- api/status.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export default function handler(req: any, res: any) {
-  res.status(200).json({ status: "running", time: new Date().toISOString() });
-}
